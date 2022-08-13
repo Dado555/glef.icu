791ec5ab145c9dfd79b945ef5bc835ab3e23afc5
Dado555 <dmalic555@gmail.com>
2022-08-13 14:51:18 +0200

    user service routes and main

--- user-service/routes/routes.go
+++ user-service/routes/routes.go
@@ -1,1 +1,27 @@
 package routes
+
+import (
+	"github.com/Dado555/glef.icu/user-service/api"
+	"github.com/Dado555/glef.icu/user-service/auth"
+	"github.com/gorilla/mux"
+	"github.com/urfave/negroni"
+	"net/http"
+)
+
+func CreateRoutes(api *api.API) *mux.Router {
+	muxRouter := mux.NewRouter()
+
+	// api
+	a := muxRouter.PathPrefix("/api").Subrouter()
+
+	// users
+	u := a.PathPrefix("/user").Subrouter()
+	u.HandleFunc("/signup", api.UserSignup).Methods("POST")
+	u.HandleFunc("/login", api.UserLogin).Methods("POST")
+	u.Handle("/info", negroni.New(
+		negroni.HandlerFunc(auth.JwtMiddleware.HandlerWithNext),
+		negroni.Wrap(http.HandlerFunc(api.UserInfo)),
+	))
+
+	return muxRouter
+}
--- user-service/server.go
+++ user-service/server.go
@@ -1,2 +1,17 @@
 package user_service
 
+import (
+	api2 "github.com/Dado555/glef.icu/user-service/api"
+	"github.com/Dado555/glef.icu/user-service/models"
+	routes2 "github.com/Dado555/glef.icu/user-service/routes"
+	"github.com/urfave/negroni"
+)
+
+func main() {
+	db := models.NewPostgresDatabase("userService.db")
+	api := api2.CreateAPI(db)
+	routes := routes2.CreateRoutes(api)
+	n := negroni.Classic()
+	n.UseHandler(routes)
+	n.Run(":3000")
+}
